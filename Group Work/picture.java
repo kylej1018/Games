d8107033ced466e1c122fdfd5cc281848100a33f
kylej1018 <kylej1018@yahoo.com>
2019-03-22 13:05:20 -0700

    Update picture.java

--- Group Work/picture.java
+++ Group Work/picture.java
@@ -9,7 +9,7 @@ public class picture extends Applet {
 		Color c = Color.yellow;
 		g.setColor(Color.red);
 		g.drawRect(10, 20, 30, 40);
-		g.setColor(Color.blue)
+		g.setColor(Color.blue);
 		g.drawOval(115, 225, 300, 40);
 		g.setColor(Color.black);
 		g.fillRect(25, 35, 30, 40);
