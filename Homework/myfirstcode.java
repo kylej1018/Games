c7a3097eece427c48bd62ce6c382cafc08812b84
kylej1018 <kylej1018@yahoo.com>
2019-03-22 12:59:35 -0700

    Add files via upload

---
+++ Homework/myfirstcode.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Random;
+public class myfirstcode {
+	public static void main (String [] args) {
+		System.out.println("This will be in its own line");
+		System.out.print("Is this all one ");
+		System.out.println("line in the code?");
+		System.out.println("Who's to say?");
+		System.out.println("Dear Kyle, ");
+		System.out.println("I'm having a really fun time with you as I learn this whole coding thing. I really love that you enjoy teaching me so many new things. I wonder what it will look like in command prompt if I just make this line really really long?");
+		System.out.println("Suppose it'll just cut off?");
+		System.out.println("Who's to say?");
+		System.out.println("	Love,");
+		System.out.println("	Sofia");
+	}
+}
