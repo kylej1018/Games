7940e9789066743ff2ceaf353501a97c4e717101
kylej1018 <kylej1018@yahoo.com>
2019-04-04 14:20:23 -0700

    Update PaintPalette.java

--- Assignments/PaintPalette.java
+++ Assignments/PaintPalette.java
@@ -3,16 +3,18 @@
 //Change brush sizes
 //Switch from circle to square and others
 //Switch from filled in to outline
+//Erase button
 
 import java.applet.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class PaintPalette extends Applet implements MouseListener, KeyListener { //use appletviewer to run
+public class PaintPalette extends Applet implements MouseListener, MouseMotionListener, KeyListener { //use appletviewer to run
 
 	public void init() {
 		addMouseListener(this);
+		addMouseMotionListener(this);
 		addKeyListener(this);
 	}
 
@@ -23,6 +25,9 @@ public class PaintPalette extends Applet implements MouseListener, KeyListener {
 	public void mouseReleased(MouseEvent e) {}
 	public void mouseEntered(MouseEvent e) {}
 	public void mouseExited(MouseEvent e) {}
+	
+	public void mouseDragged(MouseEvent e) {}
+	public void mouseMoved(MouseEvent e) {}
 
 	public void keyTyped(KeyEvent e) {}
 	public void keyPressed(KeyEvent e) {}
