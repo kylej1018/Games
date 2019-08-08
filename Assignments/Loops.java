06c4e6f023c080e8e15f77cfee6f6b3156a20a00
kylej1018 <kylej1018@yahoo.com>
2019-08-07 19:05:20 -0700

    Update Loops.java

--- Assignments/Loops.java
+++ Assignments/Loops.java
@@ -17,7 +17,7 @@ public class Loops { //does NOT use appletviewer.
 		//#4. Divide two doubles <= 1000
 		//#5. Divide two ints <= 1000 and print quotient and fraction remainder ("50/9 = 5 5/9")
 		//#6. Raise a random number <=10 to the power of another number <= 10
-		//#7. Find the factorial of a random number <= 20
+		//#7. Find the factorial of a random number <= 15
 		//#8. Find Fibonacci sequence up to a random number of iterations <= 20 (0, 1, 1, 2, 3, 5, 8, 13, 21...)
 		//#9. Find the Collatz sequence of a number <= 50
 
