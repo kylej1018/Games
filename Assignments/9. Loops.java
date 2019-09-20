b71c26dfa7bc8189c2db027a001b1abca808a628
kylej1018 <kylej1018@yahoo.com>
2019-09-20 14:14:02 -0700

    Update 9. Loops.java

--- Assignments/9. Loops.java
+++ Assignments/9. Loops.java
@@ -11,14 +11,14 @@ public class Loops {
 	public static void main (String [] args) {
 		int [] sortedIntArray1 = populateNum("sortedIntArray1"); //these three lines populate some example arrays for you to work with.  Don't mess with them.
 		int [] sortedIntArray2 = populateNum("sortedIntArray2");
-		int [] unsortedIntArray = populateNum("unsortedIntArray");/*LESSON 3 things to know before starting:
-    
-    //#15. Take sortedIntArray1 and randomize it in a new array.
+		int [] unsortedIntArray = populateNum("unsortedIntArray");
+		
+		//#15. Take sortedIntArray1 and randomize it in a new array.
 		//#16. "extractsort" unsortedIntArray. The next smallest element goes to the end in new array {1, 4, 2, 3} --> {} {1} {1, 2} {1, 2, 3} {1, 2, 3, 4}
 		//#17. "bubblesort" unsortedIntArray. Trade misplaced duos. {1, 4, 2, 3, 1} --> {1, 2, 4, 3, 1} {1, 2, 3, 4, 1} {1, 2, 3, 1, 4} {1, 2, 1, 3, 4} {1, 1, 2, 3, 4}
 		//#18. "countsort" unsortedIntArray  Find range & make a 2D array.  First dimension = all ints in range.  In second dimension, number of occurrences of each int.
 		  //{1, 5, 1, 6, 2, 2, 3, 1, 5} --> {1, 2, 3, 4, 5, 6} --> {1, 1, 1, 2, 2, 3, 5, 5, 6}
-			//                                {3, 2, 1, 0, 2, 1}
+		  //                                {3, 2, 1, 0, 2, 1}
     }
     
 	public static void printArray (int [] arr) { //this prints your arrays for you
