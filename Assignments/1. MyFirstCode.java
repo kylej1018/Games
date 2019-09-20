ad49c37a4d03a4c4d4733eecf7126f9f28a91615
kylej1018 <kylej1018@yahoo.com>
2019-09-20 12:00:49 -0700

    Update 1. MyFirstCode.java

--- Assignments/1. MyFirstCode.java
+++ Assignments/1. MyFirstCode.java
@@ -1,7 +1,9 @@
-//Assignment: Get this code to run.
-//Try putting something between the quotes in System.out.println("");
-//Try adding another System.out.print line saying something.  What is the difference between print and println?  (hint: println is pronoucnmed "print line")
-//Homework: Make it print your favorite joke.
+/*
+Assignment: Get this code to run.
+Try putting something between the quotes in System.out.println("");
+Try adding another System.out.print line saying something.  What is the difference between print and println?  (hint: println is pronoucnmed "print line")
+Homework: Make it print your favorite joke.
+*/
 public class MyFirstCode {
 	public static void main (String [] args) {
 		System.out.println("");
