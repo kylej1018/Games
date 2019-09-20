37396caaa741ac6cb30c9705f4a3483978dce8d0
kylej1018 <kylej1018@yahoo.com>
2019-09-19 18:21:01 -0700

    Update 1. myfirstcode.java

--- Assignments/1. myfirstcode.java
+++ Assignments/1. myfirstcode.java
@@ -2,7 +2,7 @@
 //Try putting something between the quotes in System.out.println("");
 //Try adding another System.out.print line saying something.  What is the difference between print and println?  (hint: println is pronoucnmed "print line")
 //Homework: Make it print your favorite joke.
-public class myfirstcode {
+public class Myfirstcode {
 	public static void main (String [] args) {
 		System.out.println("");
 		System.out.print("Hello, world!");
