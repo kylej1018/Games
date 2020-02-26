e95313eb07f735a68f756f9605385fdaa5c6cb4d
kylej1018 <kylej1018@yahoo.com>
2020-02-26 12:02:59 -0800

    Update 04. PaintPalette.java

--- Assignments/04. PaintPalette.java
+++ Assignments/04. PaintPalette.java
@@ -1,5 +1,5 @@
 /*
-Assignment: Make a drawing game like Procreate.  You should be able to click and drag the mouse and draw a "smear of paint" across your screen.
+Assignment: Make a drawing game.  You should be able to click and drag the mouse and draw a "smear of paint" across your screen.
 Suggested features:
 ~Design an original splash screen (look up what that is if you don't know)
 ~Change colors
@@ -7,8 +7,7 @@ Suggested features:
 ~Switch from circle to square
 ~Switch from filled in to outline
 ~Be able to clear screen to blank
-~Set the font to Heveltica or something that doesn't look lame for g.drawString (try to look up how)
-~Procreate and similar games have "buttons" to help the user facilitate these features.  You should too.
+~Procreate and similar games have "buttons" on the screen to help the user facilitate these features.  You should too.
 */
 
 import java.awt.*;
@@ -26,16 +25,17 @@ public class PaintPalette implements MouseListener, MouseMotionListener, KeyList
 		new PaintPalette();
 	}
 	
-	public void init() {
+	public PaintPalette() {
+		preinit();
+		init();
+	}
+	
+	public void preInit() {
 		window = new JFrame("PaintPalette");
 		pane = new JPanel() {
 			@Override
 			public void paintComponent(Graphics g) {
 				super.paintComponent(g);
-				buffer(g);
-			}
-
-			public void buffer(Graphics g) {
 				if(buffImg == null) { //for double-buffering
 					buffImg = createImage(screenwidth, screenheight);
 				}
@@ -43,10 +43,11 @@ public class PaintPalette implements MouseListener, MouseMotionListener, KeyList
 				g.drawImage(buffImg, 0, 0, pane);
 			}
 		};
-		screen = Toolkit.getDefaultToolkit().getScreenSize(); //resizes window to computer screen size
+		screen = Toolkit.getDefaultToolkit().getScreenSize(); //resizes applet to computer screen size
 		screenwidth = (int)screen.getWidth();
 		screenheight = (int)screen.getHeight();
 		window.setSize(screenwidth, screenheight);
+
 		pane.addMouseListener(this);
 		pane.addMouseMotionListener(this);
 		pane.addKeyListener(this);
