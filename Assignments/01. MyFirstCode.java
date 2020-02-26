f6a26061a29eb5135d1b05f8b3d9711ec165bc65
kylej1018 <kylej1018@yahoo.com>
2020-02-26 13:20:48 -0800

    Update 01. MyFirstCode.java

--- Assignments/01. MyFirstCode.java
+++ Assignments/01. MyFirstCode.java
@@ -1,10 +1,9 @@
 /*
 Assignment: Get this code to run.
-Homework: Make it print your favorite joke by putting something between the quotes in System.out.println("");
+Homework: Make it print your favorite joke by putting something between the quotes.
 */
 public class MyFirstCode {
 	public static void main (String [] args) {
-		System.out.println("");
 		System.out.print("Hello, world!");
 	}
 }
