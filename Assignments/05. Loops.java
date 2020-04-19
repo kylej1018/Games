2836095b561bcc91d2c5b23804bd1944686c3a9d
kylej1018 <kylej1018@yahoo.com>
2020-04-18 22:09:46 -0700

    Update 05. Loops.java

--- Assignments/05. Loops.java
+++ Assignments/05. Loops.java
@@ -1,21 +1,21 @@
 /*
-Complete all these assignments.  Note that you can do this all within main and the results will be in the console.
-Print the task # before each answer.
-Print the operation you're trying to do, with numbers, such as "67 + 103 = " so that you can check your own math.
-*/
+ * Complete all these assignments.  Note that you can do this all within main and the results will be in the console.
+ * Print the task # before each answer.
+ * Print the operation you're trying to do, with numbers, such as "67 + 103 = " so that you can check your own math.
+ */
 import java.io.*;
 import java.util.*;
 public class Loops {
 	public static void main (String [] args) {
 		Random r = new Random();
-		//#1. Sum two random numbers <= 1000
-		//#2. Subtract two random numbers <= 1000
-		//#3. Multiply two random numbers <= 100
-		//#4. Divide two doubles <= 1000
-		//#5. Divide two ints <= 1000 and print quotient and fraction remainder ("50/9 = 5 5/9")
-		//#6. Raise a random number <=10 to the power of another number <= 10
-		//#7. Find the factorial of a random number <= 15
-		//#8. Find Fibonacci sequence up to a random number of iterations <= 20 (0, 1, 1, 2, 3, 5, 8, 13, 21...) (look up what this sequence is)
-		//#9. Find the Collatz sequence of a number <= 50 (look up what this is)
+		// #1. Sum two random numbers <= 1000
+		// #2. Subtract two random numbers <= 1000
+		// #3. Multiply two random numbers <= 100
+		// #4. Divide two doubles <= 1000
+		// #5. Divide two ints <= 1000 and print quotient and fraction remainder ("50/9 = 5 5/9")
+		// #6. Raise a random number <=10 to the power of another number <= 10
+		// #7. Find the factorial of a random number <= 15
+		// #8. Find Fibonacci sequence up to a random number of iterations <= 20 (0, 1, 1, 2, 3, 5, 8, 13, 21...) (look up what this sequence is)
+		// #9. Find the Collatz sequence of a number <= 50 (look up what this is)
 	}
 }
