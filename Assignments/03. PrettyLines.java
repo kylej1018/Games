f8467f7def2ee3c4c3944a4e9656f034eb6aef74
kylej1018 <kylej1018@yahoo.com>
2020-04-18 22:08:29 -0700

    Update 03. PrettyLines.java

--- Assignments/03. PrettyLines.java
+++ Assignments/03. PrettyLines.java
@@ -1,18 +1,18 @@
 /*
-Last time, we worked only in the myPaint area.  We'll do some myPaint this time, but now you'll also be looking at global variables and Listener interfaces.
-Read through all the tutorials for section 3 before starting.
-
-Assignment:
-Debug as needed until it runs.  Once it runs, you'll find it doesn't work so great.  Solve each of these problems, one at a time.
-1. I want to click anywhere in my window and draw a circle perfectly around that point.
-2. I want to draw a line from my point to my previous point.  How can you retain the previous x and y values after clicking?
-3. I want to type "s" and "c" and have it switch between drawing a square and circle.  Still want the line though.
-4. Add something cool to this code.
-Another shape or color is cool.
-A circle radius that changes size with each click is cool.
-Text, titles, instructions is cool.
-Can you think of anything else?
-*/
+ * Last time, we worked only in the myPaint area.  We'll do some myPaint this time, but now you'll also be looking at global variables and Listener interfaces.
+ * Read through all the tutorials for section 3 before starting.
+ * 
+ * Assignment:
+ * Debug as needed until it runs.  Once it runs, you'll find it doesn't work so great.  Solve each of these problems, one at a time.
+ * 1. I want to click anywhere in my window and draw a circle perfectly around that point.
+ * 2. I want to draw a line from my point to my previous point.  How can you retain the previous x and y values after clicking?
+ * 3. I want to type "s" and "c" and have it switch between drawing a square and circle.  Still want the line though.
+ * 4. Add something cool to this code.
+ * Another shape or color is cool.
+ * A circle radius that changes size with each click is cool.
+ * Text, titles, instructions is cool.
+ * Can you think of anything else?
+ */
 
 import java.awt.*;
 import java.awt.event.*;
@@ -22,7 +22,7 @@ public class PrettyLines implements MouseListener, KeyListener {
 	JFrame window = new JFrame("PrettyLines");
 	JPanel pane;
 	
-	int x = 0; //global variables declared and initialized
+	int x = 0; // global variables declared and initialized
 	int y = 0;
 	int r = 0;
 	String shape = "circle";
@@ -61,13 +61,13 @@ public class PrettyLines implements MouseListener, KeyListener {
 	public void myPaint(Graphics g) {
 		g.setColor(color.black);
 		if(shape == "circle") {
-			g.drawOval(x, y, r*2, r*2); //draw circle
+			g.drawOval(x, y, r*2, r*2); // draw circle
 			g.drawLine(0, 0, x, y);
 		}
-		//For part 3, add code here for square
+		// For part 3, add code here for square
 	}
 
-	public void mouseClicked(MouseEvent e) { //x and y update when you click the mouse somewhere
+	public void mouseClicked(MouseEvent e) { // x and y update when you click the mouse somewhere
 		x = e.getX();
 		y = e.getY();
 		pane.repaint();
@@ -78,11 +78,11 @@ public class PrettyLines implements MouseListener, KeyListener {
 	public void mouseEntered(MouseEvent e) {}
 	public void mouseExited(MouseEvent e) {}
 
-	public void keyTyped(KeyEvent e) { //shape updates when you type the correct letter
+	public void keyTyped(KeyEvent e) { // shape updates when you type the correct letter
 		if(e.getKeyChar() == 's') {
 			shape = "square";
 		}
-		//For part 3, add code here for circle
+		// For part 3, add code here for circle
 		pane.repaint();
 	}
 	public void keyPressed(KeyEvent e) {}
