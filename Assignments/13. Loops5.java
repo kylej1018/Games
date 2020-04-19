d1a6229ba42b470e4ffde76c80884cdb77a91f56
kylej1018 <kylej1018@yahoo.com>
2020-04-18 22:24:21 -0700

    Update 13. Loops5.java

--- Assignments/13. Loops5.java
+++ Assignments/13. Loops5.java
@@ -1,38 +1,39 @@
 /*
-Fix the FIXME line to reflect where you saved the Arrays folder.
-Complete all these assignments.  Print the task # before each answer.
-Do not change anything in these arrays.
-Use printArray(arr) to print the contents of an array or ArrayList.  Remember that this command will not work for other codes.
+ * Fix the FIXME line to reflect where you saved the Arrays folder.
+ * Complete all these assignments.  Print the task # before each answer.
+ * Do not change anything in these arrays.
+ * Use printArray(arr) to print the contents of an array or ArrayList.  Remember that this command will not work for other codes.
 */
 import java.io.*;
 import java.util.*;
 public class Loops5 {
 	public static String loc = "C:\\Users\\kylej\\Desktop\\"; \\FIXME
 	public static void main (String [] args) {
-		int [] sortedIntArray1 = populateNum("sortedIntArray1"); //these three lines populate some example arrays for you to work with.  Don't mess with them.
+		// These three lines populate some example arrays for you to work with.  Don't mess with them.
+		int [] sortedIntArray1 = populateNum("sortedIntArray1");
 		int [] sortedIntArray2 = populateNum("sortedIntArray2");
 		int [] unsortedIntArray = populateNum("unsortedIntArray");
-		ArrayList <String> stringArray1 = populateStr("StringArray1"); //more example arrays.  Don't mess with them.
+		ArrayList <String> stringArray1 = populateStr("StringArray1"); // more example arrays.  Don't mess with them.
 		ArrayList <String> stringArray2 = populateStr("StringArray2");
 		ArrayList <String> keywordCipher = populateStr("KeywordCipher");
 
 		Scanner sc = new Scanner(System.in);
     
-		//#25. Prompt user input.  Translate the input to Pig Latin and print it out.
-		//#26. Prompt user input. Alphabetize each word in the sentence but don't reverse the whole sentence. {the quick brown fox} --> {eht cikqu bnorw fox}
-		//#27. Prompt user input and perform a reverse cipher
-			//original: ABCDEFGHIJKLMNOPQRSTUVWXYZ
-			//coded:    ZYXWVUTSRQPONMLKJIHGFEDCBA
-		//#28. Prompt two user inputs.  The second is a number to dictate advancement of the first in an advance cipher.
-			//original:  ABCDEFGHIJKLMNOPQRSTUVWXYZ
-			//coded (3): XYZABCDEFGHIJKLMNOPQRSTUVW
-		//#29. Prompt two user inputs.  The second is a keyword to encode the first in a keyword cipher.
-			//original: ABCDEFGHIJKLMNOPQRSTUVWXYZ
-			//coded:    SKELINGTOABCDFHJMPQRUVWXYZ (keyword = SKELLINGTON, but you can't have duplicate letters)
-		//#30. Identify the keyword in Kyle's keyword cipher.  The encoded phrase is the first element in keywordCipher.  The remaining elements in the list are possible keywords you'll have to try.  Only one will make a coherent sentence.  Print the keywords and their sentences.
+		// #25. Prompt user input.  Translate the input to Pig Latin and print it out.
+		// #26. Prompt user input. Alphabetize each word in the sentence but don't reverse the whole sentence. {the quick brown fox} --> {eht cikqu bnorw fox}
+		// #27. Prompt user input and perform a reverse cipher
+			// original: ABCDEFGHIJKLMNOPQRSTUVWXYZ
+			// coded:    ZYXWVUTSRQPONMLKJIHGFEDCBA
+		// #28. Prompt two user inputs.  The second is a number to dictate advancement of the first in an advance cipher.
+			// original:  ABCDEFGHIJKLMNOPQRSTUVWXYZ
+			// coded (3): XYZABCDEFGHIJKLMNOPQRSTUVW
+		// #29. Prompt two user inputs.  The second is a keyword to encode the first in a keyword cipher.
+			// original: ABCDEFGHIJKLMNOPQRSTUVWXYZ
+			// coded:    SKELINGTOABCDFHJMPQRUVWXYZ (keyword = SKELLINGTON, but you can't have duplicate letters)
+		// #30. Identify the keyword in Kyle's keyword cipher.  The encoded phrase is the first element in keywordCipher.  The remaining elements in the list are possible keywords you'll have to try.  Only one will make a coherent sentence.  Print the keywords and their sentences.
   }
     
-	public static int [] populateNum(String s) { //this is how the computer reads the text files.  Don't mess with this.
+	public static int [] populateNum(String s) { // this is how the computer reads the text files.  Don't mess with this.
 		ArrayList <String> arr1 = populateStr(s);
 		int [] arr2 = new int[arr1.size()];
 		for(int i = 0; i < arr1.size(); i++) {
@@ -41,7 +42,7 @@ public class Loops5 {
 		return arr2;
 	}
 
-	public static ArrayList <String> populateStr(String s) { //this is how the computer reads the text files.  Don't mess with this.
+	public static ArrayList <String> populateStr(String s) { // this is how the computer reads the text files.  Don't mess with this.
 		ArrayList <String> arr = new ArrayList<String>();
 		String loc2 = loc + "\\" + s + ".txt";
 		try {
@@ -57,14 +58,14 @@ public class Loops5 {
 		return arr;
 	}
     
-  public static void printArray (int [] arr) { //this prints your arrays for you
+  public static void printArray (int [] arr) { // this prints your arrays for you
 		for(int i = 0; i < arr.length; i++) {
 			System.out.print(" "+arr[i]);
 		}
 		System.out.println();
 	}
 
-	public static void printArray (ArrayList <String> arr) { //this prints your arrays for you
+	public static void printArray (ArrayList <String> arr) { // this prints your arrays for you
 		for(int i = 0; i < arr.size(); i++) {
 			System.out.print(" "+arr.get(i));
 		}
