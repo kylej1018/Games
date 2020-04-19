c7a0f34cc1fd7929de8d7f1e21bec49eb979542c
kylej1018 <kylej1018@yahoo.com>
2020-04-18 22:12:44 -0700

    Update 06. GuessingGame.java

--- Assignments/06. GuessingGame.java
+++ Assignments/06. GuessingGame.java
@@ -1,19 +1,19 @@
 /*
-INSTRUCTIONS
-Make an app called GuessingGame.
-
-You've should now have the ability to copy-paste the relevant statements from previous codes and modify them.  Thus, you get no code skeleton here.
-
-GuessingGame should not use KeyListneer or MouseListener.  Use Scanner.
-Use System.out.print, not myPaint.
-Make an appropriate class line and public static void main line.
-Your program should use System.out.print and Scanner and be entirely coded within main.
-When the game starts, the computer should have a random number between 1 and 100.
-It should tell the user to guess and indicate the numberical limits.
-Computer takes numerical input.
-Based on what the user guesses, the computer should provide a hint to guess bigger or smaller.
-Computer takes input again & continues to respond with hints until the user guesses correctly.
-When the number is guessed, the computer should indicate success, tell the user how many tries they took, and comment on how well they did, ideally in a humorous way.
-The computer offers to play again and prompts the user to respond (Y, N, y, n, Yes, No, YES, NO, yes, no).
-For every user line, have a while loop that waits until the user types what your'e expecting.
-*/
+ * INSTRUCTIONS
+ * Make an app called GuessingGame.
+ * 
+ * You've should now have the ability to copy-paste the relevant statements from previous codes and modify them.  Thus, you get no code skeleton here.
+ * 
+ * GuessingGame should not use KeyListener or MouseListener.  Use Scanner.
+ * Use System.out.print, not myPaint.
+ * Make an appropriate class line and public static void main line.
+ * Since your program should use System.out.print and Scanner, it should be entirely coded within main.
+ * When the game starts, the computer should have a random number between 1 and 100.
+ * It should tell the user to guess and indicate the numberical limits.
+ * Computer takes numerical input.
+ * Based on what the user guesses, the computer should provide a hint to guess bigger or smaller.
+ * Computer takes input again & continues to respond with hints until the user guesses correctly.
+ * When the number is guessed, the computer should indicate success, tell the user how many tries they took, and comment on how well they did, ideally in a humorous way.
+ * The computer offers to play again and prompts the user to respond (Y, N, y, n, Yes, No, YES, NO, yes, no).
+ * For every user line, have a while loop that only breaks if the user types what you're expecting.
+ */
