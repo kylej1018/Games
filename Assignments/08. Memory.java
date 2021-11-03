34eb2f094b57a16b3f2b80f6725609173e4570a1
kylej1018 <kylej1018@yahoo.com>
2021-11-03 11:12:08 -0700

    Update 08. Memory.java

--- Assignments/08. Memory.java
+++ Assignments/08. Memory.java
@@ -4,11 +4,11 @@
  * Start a game.
  * Computer shuffles and distributes cards, facedown, in a grid.
  * Player chooses a card, it flips over.
- * Player chooses a second card, if flips over.
+ * Player chooses a second card, it flips over.
  * If they match, cards "exit" the game (stay flipped over or move to side?)
  * If they don't match, the next click by the player, regardless of where, reverts the cards.
  * 
- * The player has a sequence of three clicks - first card, second card, revert.  Computer needs to track which we're on.
+ * The player has a sequence of three clicks - first card, second card, revert.  Computer needs a variable to track which we're on.
  * Each card has one of three values: Faceup (additional values based on size), Facedown, or Exited.
 
  * Extra optional things to add:
@@ -18,7 +18,7 @@
  * 	Game could keep track of how many times a cards is turned over.  If >2, score -5.
  * ~Button to side for rules.
  * ~Reset button: ask if easy or hard.
- * ~If hard mode, add a Joker card.  If clicked, it is removed from game but reveals all surrounding cards until next click.
+ * ~If hard mode, add a Joker card.  If clicked, it is removed from game but reveals some surrounding cards until next click.
  * 
  * Make sure to break this down into about 6-7 smaller tasks, such as
  * ~Splash & initial prompt
