03d41a0cf7a4544df1dce295c1b389ac0f2b49c3
kylej1018 <kylej1018@yahoo.com>
2021-11-03 11:27:20 -0700

    Update 01. MyFirstCode.java

--- Assignments/01. MyFirstCode.java
+++ Assignments/01. MyFirstCode.java
@@ -1,5 +1,5 @@
 /*
- * Assignment: Get this code to run.
+ * Assignment: Follow Tutorial 1a to get Java onto your computer and run this code here.
  * Homework: Make it print your favorite joke by putting something between the quotes.
  */
 public class MyFirstCode {
