d8fc04bdfe1a1142dc81a99433262d11914feb8f
kylej1018 <kylej1018@yahoo.com>
2021-11-04 12:09:35 -0700

    Update 05. Loops.java

--- Assignments/05. Loops.java
+++ Assignments/05. Loops.java
@@ -1,4 +1,4 @@
-/*
+/*INSTRUCTIONS
  * Complete all these assignments.  Note that you can do this all within main and the results will be in the console.
  * Print the task # before each answer.
  * Print the operation you're trying to do, with numbers, such as "67 + 103 = " so that you can check your own math.
